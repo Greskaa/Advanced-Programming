3fabfd025e838b1dcd765bacd1db670d2d5819a2
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2023-10-12 17:54:52 +0200

    Update and rename StructuralCode.java to Refactoring.java

---
+++ Auditory Exercises/Exercise 1/Refactoring.java
@@ -0,0 +1,37 @@
+    import java.util.Scanner;
+    
+    public class Refactoring
+    {
+        //Count all the numbers between a and b, which are divisible with the sum of their digits
+        public static int function(int a, int b)
+        {
+            int counter=0; //save the digit count
+            for (int i=a; i<=b; i++)
+            {
+                int temp = i; //save the number to calculate the sum of digits
+                int sum = 0; //save the sum of the digits;
+    
+                //Calculate the sum of digits in the number
+                while(temp > 0)
+                {
+                    sum+=(temp%10);
+                    temp/=10;
+                }
+                if (i % sum == 0)
+                    counter++;
+            }
+            return counter;
+        }
+    
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter starting number: ");
+            int x = scanner.nextInt();
+            System.out.println("Enter ending number: ");
+            int y = scanner.nextInt();
+    
+            System.out.println(function(x, y));
+    
+        }
+    }
